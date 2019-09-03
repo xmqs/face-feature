6399dbb9fe82c2a1ee065f66066b3b7ab1d51207
wyx <wuyongxing@deepexi.com>
2019-09-03 18:16:09 +0800

    2019年9月3日18:14:47 init

---
+++ .eslintrc.js
@@ -0,0 +1,82 @@
+const eslintrc = {
+  extends: [
+    'prettier',
+    'plugin:jest/recommended',
+    'plugin:react/recommended',
+    'prettier/react',
+  ],
+  env: {
+    browser: true,
+    node: true,
+    jasmine: true,
+    jest: true,
+    es6: true,
+  },
+  settings: {
+    react: {
+      version: '16.9',
+    },
+  },
+  plugins: ['markdown', 'react', 'babel', 'jest'],
+  // https://github.com/typescript-eslint/typescript-eslint/issues/46#issuecomment-470486034
+  rules: {
+    camelcase: 0,
+    'react/jsx-one-expression-per-line': 0,
+    'react/prop-types': 0,
+    'react/forbid-prop-types': 0,
+    'react/jsx-indent': 0,
+    'react/jsx-wrap-multilines': ['error', {declaration: false, assignment: false}],
+    'import/no-extraneous-dependencies': [
+      'error',
+      {
+        devDependencies: [
+          'site/**',
+          'tests/**',
+          'scripts/**',
+          '**/*.test.js',
+          '**/__tests__/*',
+          '*.config.js',
+          '**/*.md',
+        ],
+      },
+    ],
+    'jsx-a11y/no-static-element-interactions': 0,
+    'jsx-a11y/anchor-has-content': 0,
+    'jsx-a11y/click-events-have-key-events': 0,
+    'jsx-a11y/anchor-is-valid': 0,
+    'comma-dangle': ['error', 'always-multiline'],
+    'react/jsx-filename-extension': 0,
+    'react/state-in-constructor': 0,
+    'react/jsx-props-no-spreading': 0,
+    'prefer-destructuring': 0, // TODO: remove later
+    'consistent-return': 0, // TODO: remove later
+    'no-return-assign': 0, // TODO: remove later
+    'no-param-reassign': 0, // TODO: remove later
+    'react/destructuring-assignment': 0, // TODO: remove later
+    'react/no-did-update-set-state': 0, // TODO: remove later
+    'react/require-default-props': 0,
+    'react/default-props-match-prop-types': 0,
+    'import/no-cycle': 0,
+    'react/no-find-dom-node': 0,
+    'no-underscore-dangle': 0,
+    // label-has-for has been deprecated
+    // https://github.com/evcohen/eslint-plugin-jsx-a11y/blob/master/docs/rules/label-has-for.md
+    'jsx-a11y/label-has-for': 0,
+    // for (let i = 0; i < len; i++)
+    'no-plusplus': 0,
+    // https://eslint.org/docs/rules/no-continue
+    // labeledLoop is conflicted with `eslint . --fix`
+    'no-continue': 0,
+    'react/display-name': 0,
+    // ban this for Number.isNaN needs polyfill
+    'no-restricted-globals': 0,
+    'max-classes-per-file': 0,
+    'react/static-property-placement': 0,
+  },
+  globals: {
+    gtag: true,
+  },
+};
+
+
+module.exports = eslintrc;
